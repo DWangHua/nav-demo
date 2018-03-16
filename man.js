86793c5fd2080289935c9b540fd893ee1cba959a
Fanhua <744118724@qq.com>
2018-03-16 17:08:52 +0800

    函数

---
+++ man.js
@@ -0,0 +1,99 @@
+var kbds = (function() {
+
+  var consts = (function() {
+    var keys = [
+      ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+      ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+      ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    ];
+  
+    var keyUrlMap = {
+      q: 'www.qiniu.com',
+      w: 'www.weibo.com',
+      e: 'www.ele.me',
+      t: 'jx.tmall.com',
+      y: 'youtube.com',
+      u: 'www.uc.cn',
+      i: 'www.iqiyi.com',
+      o: 'opera.com',
+      p: 'www.pinduoduo.com',
+      z: 'zhihu.com',
+      m: 'www.mcdonalds.com.cn',
+      j: 'juejin.im',
+      x: 'xiedaimala.com',
+      b: 'www.baidu.com',
+      v: 'www.v2ex.com'
+    };
+    var kdbRowClsName = 'kbd-row';
+    var kbdClsName = 'kbd-item';
+    var defaultIcon = '//i.loli.net/2017/11/10/5a05afbc5e183.png';
+    return {
+      getKeys: function() { return keys; },
+      getKeyUrlMap: function() { return keyUrlMap },
+      getKbdClsName: function() { return kbdClsName; },
+      getKbdRowClsName: function() { return kdbRowClsName; },
+      getDefaultIcon: function() { return defaultIcon; }
+    }
+  })();
+
+  function init(el) {
+    if (!isElement(el)) throw new Error('please give a dom element');
+    var keys = consts.getKeys();
+    for(var i = 0; i < keys.length; i++) {
+      el.appendChild(renderRow(keys[i]));
+    }
+    document.addEventListener('keypress', keyPressListener);
+  }
+
+  function renderRow(list) {
+    var len = list.length;
+    var div = tag('div');
+    div.className = consts.getKbdRowClsName();
+    var kbd;
+    for(var i = 0; i < len; i++) {
+      kbd = renderKbd(list[i]);
+      div.appendChild(kbd);
+    }
+    return div;
+  }
+
+  function renderKbd(item) {
+    var kbd = tag('kbd');
+    var span = tag('span');
+    var img = new Image();
+    var domain = consts.getKeyUrlMap()[item]
+    var defaultIcon = consts.getDefaultIcon();
+    var iconSrc;
+    iconSrc = domain ? 'http://' + domain + '/favicon.ico' : defaultIcon;
+    img.src = iconSrc;
+    img.onerror = function(e) {
+      e.target.src = defaultIcon;
+    };
+    img.width = 18;
+    img.height = 18;
+    kbd.className = consts.getKbdClsName();
+    span.textContent = item;
+    kbd.appendChild(span);
+    kbd.appendChild(img);
+    return kbd;
+  }
+
+  function tag(tag) {
+    return document.createElement(tag);
+  }
+
+  function isElement(el) {
+    return el.nodeType !== undefined && el.nodeType === 1;
+  }
+
+  function keyPressListener(e) {
+    var key = e.key;
+    var domain = consts.getKeyUrlMap()[key];
+    domain ? window.open('http://' + domain, '_blank') : alert('暂无对应网址');
+    e.preventDefault();
+  }
+
+  return {
+    init: init
+  };
+})();  
